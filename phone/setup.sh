c887ad4b1fccc431b0db391a966d9bf14c39857d
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-10 10:00:54 -0600

    updated setup

--- phone/setup.sh
+++ phone/setup.sh
@@ -2,16 +2,20 @@
 
 # make sure we're in the right directory and up-to-date
 cd $HOME
+echo "updating software"
 pkg upgrade
 
+
+# install all programs
+echo "installing new packages"
+xargs -a $HOME/termux_dotfiles/phone/installed-pkgs.txt pkg install
+
 # set up storage
-pkg install termux-setup-storage
+echo "setting up storage"
 mkdir storage
 echo "y" | termux-setup-storage
 
-# install all programs
-xargs -a $HOME/termux_dotfiles/phone/installed-pkgs.txt pkg install
-
 # stow dotfiles
+echo "stowing configs"
 rm $HOME/../usr/etc/bash.bashrc
 stow -t /data/data/com.termux/files * 2>&1 | grep -A 2 -E "^WARNING"
